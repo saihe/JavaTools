994e3fdaeae8ed61c0d1bbfc6f3c6d9df47afc87
Kyohei Saito <saihe.kyon@gmail.com>
2020-02-11 22:47:10 +0900

    jpgとpngそれぞれ採番するようにした

--- renameFiles/src/main/java/ksaito/renameFiles/Executor.java
+++ renameFiles/src/main/java/ksaito/renameFiles/Executor.java
@@ -21,38 +21,47 @@ class Executor {
     System.out.println("対象パス：" + inputDirPath);
     try {
       Path target = Paths.get(inputDirPath);
-      AtomicInteger counter = new AtomicInteger(0);
+      AtomicInteger jpgCounter = new AtomicInteger(0);
+      AtomicInteger pngCounter = new AtomicInteger(0);
 
       if (Files.isDirectory(target)) {
         System.out.println("内部ファイル取得");
         System.out.println("取得ファイル数：" + Files.list(target).count());
         Optional.ofNullable(Files.walk(target, 2))
           .ifPresent(pathStream ->
-              pathStream
-                .sorted(Comparator.naturalOrder())
-                .filter(path -> !Files.isDirectory(path))
-                .filter(path ->  path.toString().endsWith(".jpg") || path.toString().endsWith("png"))
-                .forEach(path -> {
-                  System.out.println("処理ファイル名：" + path.getFileName());
-                  try {
-                    Files.move(
-                      path
-                      , Paths.get(
-                        inputDirPath
-                        , StringUtils.leftPad(
-                          String.valueOf(counter.incrementAndGet())
-                          , 3
-                          , "0")
-                      )
-                      , StandardCopyOption.ATOMIC_MOVE
-                    );
-                  } catch (IOException e) {
-                    e.printStackTrace();
-                    System.out.println("異常終了");
-                    System.exit(1);
-                  }
-                })
+            pathStream
+              .sorted(Comparator.naturalOrder())
+              .filter(path -> !Files.isDirectory(path))
+              .filter(path -> path.toString().endsWith(".jpg") || path.toString().endsWith(".png"))
+              .forEach(path -> {
+                String fileName = path.getFileName().toString();
+                System.out.println("処理ファイル名：" + fileName);
+                Path movedPath = Paths.get(
+                  inputDirPath
+                  , StringUtils.leftPad(
+                    String.valueOf(
+                      fileName.endsWith(".jpg") ? jpgCounter.incrementAndGet() : pngCounter.incrementAndGet()
+                    )
+                    , 3
+                    , "0") + fileName.substring(fileName.lastIndexOf(".")
+                  )
                 );
+                System.out.println("リネーム後ファイル名：" + movedPath);
+
+                try {
+                  Files.move(
+                    path
+                    , movedPath
+                    , StandardCopyOption.ATOMIC_MOVE
+
+                  );
+                } catch (IOException e) {
+                  e.printStackTrace();
+                  System.out.println("異常終了");
+                  System.exit(1);
+                }
+              })
+          );
       } else {
         throw new Exception("指定されたパスがディレクトリではない（" + inputDirPath + "）");
       }
