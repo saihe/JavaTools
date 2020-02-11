4592045bc84d0c2f9d1b6b6b64fdd498b042b2af
Kyohei Saito <saihe.kyon@gmail.com>
2020-02-11 22:12:54 +0900

    指定したディレクトリを第２階層まで再起的にファイルを取得するようにした

--- renameFiles/src/main/java/ksaito/renameFiles/Executor.java
+++ renameFiles/src/main/java/ksaito/renameFiles/Executor.java
@@ -8,7 +8,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
-import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -18,42 +17,51 @@ class Executor {
   private String inputDirPath;
 
   void run() {
+    System.out.println("開始");
+    System.out.println("対象パス：" + inputDirPath);
     try {
       Path target = Paths.get(inputDirPath);
       AtomicInteger counter = new AtomicInteger(0);
 
-      if (target.toFile().isDirectory()) {
-        Optional.ofNullable(target.toFile().listFiles()).ifPresent(files -> {
-          Arrays.stream(files)
-                  .sorted(Comparator.naturalOrder())
-                  .filter(file -> file.toPath().endsWith(".jpg") || file.toPath().endsWith(".png"))
-                  .forEach(file -> {
-                    System.out.println("処理ファイル名：" + file.getName());
-                    try {
-                      Files.move(
-                              file.toPath()
-                              , Paths.get(
-                                      inputDirPath
-                                      , StringUtils.leftPad(
-                                              String.valueOf(counter.incrementAndGet())
-                                              , 3
-                                              , "0")
-                              )
-                              , StandardCopyOption.ATOMIC_MOVE
-                      );
-                    } catch (IOException e) {
-                      e.printStackTrace();
-                      System.exit(1);
-                    }
-                  });
-        });
+      if (Files.isDirectory(target)) {
+        System.out.println("内部ファイル取得");
+        System.out.println("取得ファイル数：" + Files.list(target).count());
+        Optional.ofNullable(Files.walk(target, 2))
+          .ifPresent(pathStream ->
+              pathStream
+                .sorted(Comparator.naturalOrder())
+                .filter(path -> !Files.isDirectory(path))
+                .filter(path ->  path.toString().endsWith(".jpg") || path.toString().endsWith("png"))
+                .forEach(path -> {
+                  System.out.println("処理ファイル名：" + path.getFileName());
+                  try {
+                    Files.move(
+                      path
+                      , Paths.get(
+                        inputDirPath
+                        , StringUtils.leftPad(
+                          String.valueOf(counter.incrementAndGet())
+                          , 3
+                          , "0")
+                      )
+                      , StandardCopyOption.ATOMIC_MOVE
+                    );
+                  } catch (IOException e) {
+                    e.printStackTrace();
+                    System.out.println("異常終了");
+                    System.exit(1);
+                  }
+                })
+                );
       } else {
-        throw new Exception("指定されたパスがディレクトリではない");
+        throw new Exception("指定されたパスがディレクトリではない（" + inputDirPath + "）");
       }
 
+      System.out.println("終了");
       System.exit(0);
     } catch (Exception e) {
       e.printStackTrace();
+      System.out.println("異常終了");
       System.exit(1);
     }
   }
